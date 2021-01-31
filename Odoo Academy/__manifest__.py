af974dd603ae54bd3ea07117affea5411128a82b
dionius1981 <dionius1981@gmail.com>
2021-01-31 11:10:58 +0400

    First commit

---
+++ Odoo Academy/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'version': '0.1',
+    'description': """Academy module for manage Training""",
+    'category': 'Training',
+    'author': 'VKH',
+
+    'depends': ['base'],
+    'data': [
+        # data
+
+        # views
+    ],
+    'demo': [
+
+    ],
+}
